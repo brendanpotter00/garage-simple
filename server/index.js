b275f4c2fa27001d9667e2e203fe4de8ad6424b5
bpotter-trinity <brendanpotter00@gmail.com>
2021-07-21 21:21:24 -0500

    deploy

--- server/index.js
+++ server/index.js
@@ -16,8 +16,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+  res.send('hello to garage api');
+})
+
 //const CONNECTION_URL = 'mongodb+srv://user-123:123@cluster0.ns4qq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
