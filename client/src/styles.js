ee6187c163b3aa710544335a84b8206d555b4e01
bpotter-trinity <brendanpotter00@gmail.com>
2021-07-21 20:49:02 -0500

    ready for deployment

---
+++ client/src/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'rgb(125, 6, 51)',
+  },
+  heading: {
+    color: '#5E8B7E',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
